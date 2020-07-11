b626342526af08d9936514d0fc9b55e9054aeabb
Dan <dan.maple@outlook.com>
2020-07-11 16:37:09 +0100

    Create a Job model to be stored in PostgreSQL.

---
+++ CoreApi/Models/Job.cs
@@ -0,0 +1,17 @@
+﻿namespace CoreApi.Models
+{
+    public enum JobStatus
+    {
+        Created,
+        Scheduled,
+        Started,
+        Completed
+    }
+
+    public class Job
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+        public JobStatus status { get; set; }
+    }
+}
