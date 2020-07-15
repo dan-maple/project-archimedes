c4be60c787c55755d5da1e24756cc07fbb914a1b
Dan <dan.maple@outlook.com>
2020-07-15 20:19:51 +0100

    Initialise database.

--- CoreApi/Program.cs
+++ CoreApi/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +14,26 @@ namespace CoreApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+
+                ApiDbContext     context  = services.GetRequiredService<ApiDbContext>();
+                ILogger<Program> logger   = services.GetRequiredService<ILogger<Program>>();
+
+                try
+                {
+                    DbInitialiser.Initialise(context);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occured creating the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
