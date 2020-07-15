2ca823f4b39abd3375c84de5144a97071593f13d
Dan <dan.maple@outlook.com>
2020-07-15 20:20:22 +0100

    Create job controller.

---
+++ CoreApi/Controllers/JobController.cs
@@ -0,0 +1,26 @@
+﻿using CoreApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        private ApiDbContext _context;
+
+        public JobController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Job>> Get()
+        {
+            List<Job> jobs = _context.Jobs.ToList();
+            return jobs;
+        }
+    }
+}
