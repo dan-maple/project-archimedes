40e3e256055f1f4c63ce44b4e577f69ea9e5b3da
Dan <dan.maple@outlook.com>
2020-07-25 18:54:35 +0100

    Correct job DTO.

--- CoreApi/Dtos/JobDto.cs
+++ CoreApi/Dtos/JobDto.cs
@@ -6,15 +6,12 @@ namespace CoreApi.Dtos
 {
     public class JobDto
     {
-        public class Job
-        {
-            public long Id { get; set; }
+        public long Id { get; set; }
 
-            [Required]
-            public string Description { get; set; }
+        [Required]
+        public string Description { get; set; }
 
-            [DefaultValue(JobStatus.Created)]
-            public JobStatus Status { get; set; }
-        }
+        [DefaultValue(JobStatus.Created)]
+        public JobStatus Status { get; set; }
     }
 }
