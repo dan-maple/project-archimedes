8dec9ab55e969a3161310d32178fb9bc2a9b24e1
Dan <dan.maple@outlook.com>
2020-07-25 18:56:38 +0100

    Create migration to add 'Cost' to job model.

--- CoreApi/Migrations/20200725164110_UpdateJob_Cost.Designer.cs
+++ CoreApi/Migrations/20200725164110_UpdateJob_Cost.Designer.cs
@@ -2,15 +2,17 @@
 using CoreApi;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace CoreApi.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200725164110_UpdateJob_Cost")]
+    partial class UpdateJob_Cost
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CoreApi/Migrations/20200725164110_UpdateJob_Cost.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreApi.Migrations
+{
+    public partial class UpdateJob_Cost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Cost",
+                table: "Jobs",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "Jobs");
+        }
+    }
+}
